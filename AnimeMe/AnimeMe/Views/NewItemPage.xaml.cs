e872bd89b492d984dfba2176ba8355e64bc40f0c
Kevin <kevinvosburgh4@gmail.com>
2020-09-10 19:33:23 -0400

    base of beginning

---
+++ AnimeMe/AnimeMe/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AnimeMe.Models;
+using AnimeMe.ViewModels;
+
+namespace AnimeMe.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
